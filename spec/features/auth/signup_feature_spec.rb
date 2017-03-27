2c2c0b60a90a6e0ecd8f1baf75d3cb323f9aacc6
Krishna <krishnaklooper@gmail.com>
2017-03-28 00:06:05 +0530

    deret13'

--- spec/features/auth/signup_feature_spec.rb
+++ spec/features/auth/signup_feature_spec.rb
@@ -144,10 +144,7 @@ feature "Signup feature" do
   end
 
   scenario "Submit Button gets disabled when any field is filled wrong", js: true do
-    visit new_user_registration_url
-    page.execute_script("$('#new_user').show()")
-    page.execute_script("$('#submit_btn').click()")
-    page.execute_script("$('#user_username').show()")
+    visit '/users/sign_up'
     wait_for_effect_on("#new_user")
     fill_in "Username", with: user.username
     fill_in "user_email", with: "gibberish"
