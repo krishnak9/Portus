d6c49c6bf7b185a6f2a7b3bc248e675fb8628ef4
Krishna Kumar <krishnaklooper@gmail.com>
2017-04-09 13:23:18 +0530

    introducing keyup

--- app/assets/javascripts/modules/users/components/signup-form.js
+++ app/assets/javascripts/modules/users/components/signup-form.js
@@ -15,39 +15,78 @@ class UsersSignUpForm extends BaseComponent {
     this.$username = this.$el.find(USERNAME_FIELD);
     this.$userEmail = this.$el.find(EMAIL_FIELD);
     this.$submit = this.$el.find(SUBMIT_BUTTON);
+    this.$pass_valid = false;
+    this.$pass_conf_valid = false;
+    this.$username_valid = false;
+    this.$email_valid = false;
   }
 
   events() {
-    this.$el.on('focusout', PASSWORD_FIELD, e => this.onFocusout(e));
-    this.$el.on('focusout', CONFIRMATION_PASSWORD_FIELD, e => this.onFocusout(e));
-    this.$el.on('focusout', USERNAME_FIELD, e => this.onFocusout(e));
-    this.$el.on('focusout', EMAIL_FIELD, e => this.onFocusout(e));
+    this.$el.on('keyup', PASSWORD_FIELD, e => this.onkeyuppassword(e));
+    this.$el.on('keyup', CONFIRMATION_PASSWORD_FIELD, e => this.onkeyuppassword_conf(e));
+    this.$el.on('keyup', USERNAME_FIELD, e => this.onkeyupusername(e));
+    this.$el.on('keyup', EMAIL_FIELD, e => this.onkeyupemail(e));
   }
 
-  onFocusout() {
+  onkeyupusername() {
     const usernameInvalid = !this.$username[0].validity.valid;
+
+    if (usernameInvalid) {
+      this.$username_valid = false;
+    } else {
+      this.$username_valid = true;
+    }
+
+    allvalidation();
+  }
+
+  onkeyupemail() {
     const emailInvalid = !this.$userEmail[0].validity.valid;
-    const passwordInvalid = !this.$password.val() || this.$password.val().length < 8;
+
+    if (emailInvalid) {
+      this.$email_valid = false;
+    } else {
+      this.$email_valid = true;
+    }
+
+    allvalidation();
+  }
+
+  onkeyuppassword_conf() {
     const passwordConfirmationInvalid = !this.$passwordConfirmation.val() ||
       this.$password.val() !== this.$passwordConfirmation.val();
 
     if (passwordConfirmationInvalid) {
       this.$passwordConfirmation[0].setCustomValidity('Please enter same password as above');
+      this.$pass_conf_valid = false;
     } else {
       this.$passwordConfirmation[0].setCustomValidity('');
+      this.$pass_conf_valid = true;
     }
 
+    allvalidation();
+  }
+
+  onkeyuppassword() {
+    const passwordInvalid = !this.$password.val() || this.$password.val().length < 8;
+    
     if (passwordInvalid) {
       this.$password[0].setCustomValidity('Password should be of at least 8 characters minimum.');
+      this.$pass_valid = false;
     } else {
       this.$password[0].setCustomValidity('');
       this.$passwordConfirmation.attr('pattern', this.$password.val());
+      this.$pass_valid = true;
     }
 
-    if (passwordInvalid || passwordConfirmationInvalid || emailInvalid || usernameInvalid) {
-      this.$submit.attr('disabled', true);
-    } else {
+    allvalidation();
+  }
+
+  allvalidation() {
+    if (this.$pass_valid || this.pass_conf_valid || this.$email_valid || this.$username_valid) {
       this.$submit.removeAttr('disabled');
+    } else {
+      this.$submit.attr('disabled', true);
     }
   }
 }
--- spec/features/auth/signup_feature_spec.rb
+++ spec/features/auth/signup_feature_spec.rb
@@ -143,6 +143,12 @@ feature "Signup feature" do
                                   text: 'Password confirmation doesn\'t match Password')
   end
 
+  scenario "Submit Button is disabled on visiting the signup page", js: true do
+    visit "/users/sign_up"
+    wait_for_effect_on("#submit_btn")
+    expect(page).to have_button("submit_btn", disabled: true)
+  end
+
   scenario "Submit Button gets disabled when any field is filled wrong", js: true do
     visit "/users/sign_up"
     wait_for_effect_on("#new_user")
